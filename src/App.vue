cb2326a34d323fc3e43bb1268fdf762b12d131f0
gmmmansuri <gulammustafa.mansuri@wohlig.com>
2020-02-04 11:00:48 +0530

    first git

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,104 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      {{ massage }}
+    </div>
+    <div>
+      <span v-bind:title="massage">
+        hi gulam mustafa jhjhjkh
+      </span>
+    </div>
+    <div>
+      <p v-if="gulam">if directive check</p>
+
+      <!-- <pre>{{todos}}</pre> -->
+      <div>
+        <ol>
+          <li v-for="(x, index) in todos" :key="index">
+            {{ x.name }}{{ index }}
+          </li>
+        </ol>
+      </div>
+
+      <div>
+        <p>{{ massage }}</p>
+        <button @click="firstfunction()">reverse massage</button>
+      </div>
+    </div>
+    <div>
+      <p>
+        <input type="text" v-model="massage">
+      </p>
+    </div>
+    <p>{{info}}</p>
+    <div>
+      <helloWorld  v-for='(todo,index) in todos' :key='index'>
+      
+      </helloWorld>
+    </div>
+    <img alt="Vue logo" src="./assets/logo.png" />
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Vue from 'vue'
+import axios from 'axios'
+import helloWorld from '@/components/helloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  components:{
+helloWorld
+  },
+  data() {
+    return {
+      massage: "hellow vue",
+      info:null,
+      gulam: true,
+      todos: [
+        {
+          name: "gulam",
+          age: 27,
+          gender: "M"
+        },
+        {
+          name: "mustafa",
+          age: 24,
+          gender: "M"
+        },
+        {
+          name: "nayan",
+          age: 27,
+          gender: "M"
+        },
+        {
+          name: "dimple",
+          age: 20,
+          gender: "F"
+        }
+      ]
+    };
+  },
+  methods: {
+    firstfunction: function() {
+      this.massage = this.massage
+        .split("")
+        .reverse()
+        .join("");
+    }
+  },
+  mounted(){
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
   }
-}
+  
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/helloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>helo gulam from hello world</p>
+    <!-- <span>{{todo.name}}</span> -->
+  </div>
+</template>
+<script>
+export default {
+  name: "helloWorld",
+  props: {
+    parentData: Object,
+    stringProp: String,
+    title: String
+  },
+
+  data() {
+    return {
+      formData: "jkhjhjkhjkh"
+    };
+  }
+};
+</script>
